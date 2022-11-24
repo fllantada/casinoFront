145371df5e26254c2d93140a2f1cae90d6ca98b6
fllantada <franciscollantada@gmail.com>
2022-11-24 09:38:55 -0300

    typescript bug any temporaly solution

--- components/TableSlots.tsx
+++ components/TableSlots.tsx
@@ -8,20 +8,20 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
 
-export default function BasicTable({ one, two, three }) {
+export default function BasicTable(props: any) {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableBody>
           <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
             <TableCell key={1} align='center'>
-              <Typography variant='h1'>{one}</Typography>
+              <Typography variant='h1'>{props.one}</Typography>
             </TableCell>
             <TableCell key={2} align='center'>
-              <Typography variant='h1'>{two}</Typography>
+              <Typography variant='h1'>{props.two}</Typography>
             </TableCell>
             <TableCell key={3} align='center'>
-              <Typography variant='h1'>{three}</Typography>
+              <Typography variant='h1'>{props.three}</Typography>
             </TableCell>
           </TableRow>
         </TableBody>
